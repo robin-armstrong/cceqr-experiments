6b09ca8cc2bb33b874044c5ad33b5971cd7ce4d3
Robin Armstrong <rja243@cornell.edu>
2024-08-21 10:54:28 -0400

    Added code for TCPQR algorithm.

---
+++ src/algorithms/tcpqr.jl
@@ -0,0 +1,164 @@
+using LinearAlgebra
+
+include("tcpqr_utils.jl")
+
+"""
+tcpqr!(A; k = minimum(size(A)), mu = .9)
+
+Compute the first `k` entries of the column permutation for act CPQR factorization
+of `A`, modifying `A` in place. Use thresholded updates with relative threshold `mu`.
+See also `tcpqr`.
+"""
+function tcpqr!(A::Matrix{Float64}; k::Int64 = minimum(size(A)), mu::Float64 = .9)
+    m, n = size(A)
+
+    if(k < 1)
+        throw(ArgumentError("k must be a positive integer"))
+    elseif(k > min(m, n))
+        throw(ArgumentError("k cannot exceed the smaller dimension of the input matrix"))
+    elseif(mu*(1 - mu) <= 0)
+        throw(ArgumentError("mu must lie strictly between 0 and 1"))
+    end
+
+    gamma = zeros(n)         # residual column norms
+    lower = 0.               # lower bound on maximum residual column norm
+    jpvt  = Array(1:n)       # column pivot vector
+    skel  = 0                # number of skeleton columns chosen so far
+    act   = n                # number of currently active columns
+    V     = zeros(m, k)      # storage for Householder reflectors
+    T     = zeros(k, k)      # storage for T factor in compact WY representation
+
+    tmpcol = zeros(m)
+
+    # determine column norms
+
+    for j = 1:n
+        gamma[j] = norm(A[:,j])^2
+        
+        if(gamma[j] >= lower)
+            lower = gamma[j]
+        end
+    end
+    
+    # compute column permutation in cycles
+
+    cycle = 0
+
+    while(skel < k)
+        cycle += 1
+        delta  = mu*lower
+
+        # select a block from the active set to factorize
+
+        b = reblock!(A, jpvt, skel+1, act, gamma, delta)
+        (cycle == 1) && (act = b)
+
+        # factorize candidate columns with DGEQP3
+
+        block   = A[(skel+1):m, (skel+1):(skel+b)]
+        min_dim = minimum(size(block))
+        qrobj   = qr!(block, ColumnNorm())
+
+        # figure out how many pivot choices are usable
+
+        t = sum(diag(qrobj.factors).^2 .>= delta)
+        t = min(t, k-skel)
+
+        # permute columns accordingly
+
+        swap_cols!(A, jpvt, gamma, skel+1, qrobj.p)
+
+        # check to see if we've chosen enough skeleton columns
+
+        (skel+t == k) && break
+
+        # update the orthogonal factor
+
+        V[(skel+1):m, (skel+1):(skel+t)] = qrobj.factors[:, 1:t]
+        
+        for i = (skel+1):(skel+t)
+            V[i, i] = 1.
+            fill!(view(V, 1:(i-1), i), 0.)
+        end
+
+        tau = qrobj.Q.τ[1:t]
+        fill_t!(T, V, tau, skel+1, skel+t)
+
+        # apply new reflectors to the active set
+        
+        apply_qt!(A, V, T, skel+1, skel+t, skel+1, act)     # CYCLE 2: this is somehow modifying A in rows ABOVE index skel+1.
+
+        # determine a lower bound on the maximum column norm orthogonal
+        # to the range of A[:, 1:(skel+t)]
+
+        if(t == min_dim)
+            # in this case, all the columns in the block we just
+            # factored will be committed to the skeleton.
+
+            j   = skel+t+1    # first column index not in the block
+            col = reshape(A[:, j], (m, 1))
+
+            if(j > act)
+                # case where column j is not in the active set;
+                # we must apply all previous reflectors to it.
+
+                apply_qt!(col, V, T, 1, skel+t, 1, 1)
+                lower = norm(col[(skel+t+1):m])^2
+            else
+                # case where column j is in the active set, and has
+                # already had reflectors applied to it.
+
+                lower = norm(col[(skel+t+1):m])^2
+            end
+        else
+            # in this case, there were columns in the block we just
+            # factored will not be committed to the skeleton.
+
+            lower = 0.
+
+            for j = (skel+t+1):(skel+b)
+                col   = view(A, (skel+t+1):m, j)
+                lower = max(lower, norm(col)^2)
+            end
+        end
+
+        # bring new columns into the active set and apply reflectors.
+
+        act_old = act
+        act    += reblock!(A, jpvt, act_old+1, n, gamma, mu*lower)
+
+        if(act > act_old)
+            apply_qt!(A, V, T, 1, skel+t, act_old+1, act)
+        end
+
+        # update residual column norms
+
+        lower = 0.
+
+        for j = (skel+t+1):act_old
+            colview   = view(A, (skel+1):(skel+t), j)
+            gamma[j] -= norm(colview)^2
+            lower     = max(lower, gamma[j])
+        end
+
+        for j = (act_old+1):act
+            colview   = view(A, 1:(skel+t), j)
+            gamma[j] -= norm(colview)^2
+            lower     = max(lower, gamma[j])
+        end
+
+        skel += t
+    end
+
+    return jpvt[1:k]
+end
+
+"""
+tcpqr(A; k = minimum(size(A)), mu = .9)
+
+Compute the first `k` entries of the column permutation for act CPQR factorization of `A`,
+using thresholded updates with relative threshold `mu`. See also `tcpqr!`.
+"""
+function tcpqr(A::Matrix{Float64}; k::Int64 = minimum(size(A)), mu::Float64 = .9)
+    return tcpqr!(deepcopy(A); k = k, mu = mu)
+end
---
+++ src/algorithms/tcpqr_utils.jl
@@ -0,0 +1,84 @@
+using LinearAlgebra
+
+function fill_t!(T::AbstractMatrix{Float64}, V::Matrix{Float64}, tau::Vector{Float64}, p::Int64, q::Int64)
+    # filling the (p:q, p:q) block of T
+    for s = 1:(q-p+1)
+        i       = p+s-1
+        T[i, i] = tau[s]
+        
+        if i > p
+            c      = view(T, p:(i-1), i)
+            V_prev = view(V, :, p:(i-1))
+            V_new  = view(V, :, i)
+            T_prev = UpperTriangular(view(T, p:(i-1), p:(i-1)))
+
+            mul!(c, V_prev', V_new, -tau[s], 0.)
+            lmul!(T_prev, c)
+        end
+    end
+    
+    # filling the (1:(p-1), p:q) block of T
+    if p > 1
+        T11 = UpperTriangular(view(T, 1:(p-1), 1:(p-1)))
+        T22 = UpperTriangular(view(T, p:q, p:q))
+        T12 = view(T, 1:(p-1), p:q)
+        V1  = view(V, :, 1:(p-1))
+        V2  = view(V, :, p:q)
+
+        mul!(T12, V1', V2, -1., 0.)
+        lmul!(T11, T12)
+        rmul!(T12, T22)
+    end
+end
+
+function apply_qt!(A::Matrix{Float64}, V::Matrix{Float64}, T::Matrix{Float64}, p::Int64, q::Int64, r::Int64, s::Int64)
+    m, n  = size(A)
+    A1    = view(A, p:q, r:s)
+    A2    = view(A, (q+1):m, r:s)
+    T_sub = view(T, p:q, p:q)
+    V1    = view(V, p:q, p:q)
+    V2    = view(V, (q+1):m, p:q)
+    W     = Matrix{Float64}(A[p:q, r:s]')
+
+    rmul!(W, LowerTriangular(V1))
+    mul!(W, A2', V2, 1., 1.)
+    rmul!(W, UpperTriangular(T_sub))
+    mul!(A1, V1, W', -1., 1.)
+    mul!(A2, V2, W', -1., 1.)
+end
+
+function reblock!(A::Matrix{Float64}, jpvt::Vector{Int64}, j_start::Int64, j_end::Int64, gamma::Vector{Float64}, delta::Float64)
+    m, n   = size(A)
+    tmpcol = zeros(m)
+    blk    = 0
+
+    for j = j_start:j_end
+        if gamma[j] > delta
+            blk += 1
+            p    = j_start+blk-1
+            
+            tmp     = jpvt[p]
+            jpvt[p] = jpvt[j]
+            jpvt[j] = tmp
+
+            tmp      = gamma[p]
+            gamma[p] = gamma[j]
+            gamma[j] = tmp
+
+            tmpcol[:] = A[:,p]
+            A[:,p]    = A[:,j]
+            A[:,j]    = tmpcol
+        end
+    end
+
+    return blk
+end
+
+function swap_cols!(A::Matrix{Float64}, jpvt::Vector{Int64}, gamma::Vector{Float64}, j0::Int64, perm::Vector{Int64})
+    n = size(A, 2)
+    b = length(perm)
+
+    permute!(view(jpvt, j0:(j0+b-1)), perm)
+    permute!(view(gamma, j0:(j0+b-1)), perm)
+    Base.permutecols!!(view(A, :, j0:(j0+b-1)), perm)
+end
