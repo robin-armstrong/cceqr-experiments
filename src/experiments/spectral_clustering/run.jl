14e4cf98fa8619cf27b4c543b77b46dd88a19244
Robin Armstrong <rja243@cornell.edu>
2024-10-08 16:46:51 -0400

    More work done on clustering experiment; changed to Gaussian mixture model learning.

--- src/experiments/spectral_clustering/run.jl
+++ src/experiments/spectral_clustering/run.jl
@@ -14,12 +14,13 @@ include("../../algorithms/rpchol.jl")
 
 rng = MersenneTwister(2)
 
-k        = 4
-noise    = .05
+k        = 2
+scale    = 10.
+noise    = .5
 n_points = 4*round.(Int64, exp10.(range(3, 3, 1)))     # range of dataset sizes
 
-kernel    = "gaussian"  # type of kernel function      
-bandwidth = .1         # bandwidth of kernel function
+kernel    = "inv-1"  # type of kernel function      
+bandwidth = .5         # bandwidth of kernel function
 
 eta       = 1e-4        # controls threshold value for CCEQR
 rho       = 1e-4        # controls selection of columns for Householder reflection
@@ -45,13 +46,14 @@ if !plot_only
         flush(stdout)
     end
 
-    function run_cluster_experiment(rng, k, noise, n_points, kernel,
-                                    bandwidth, eta, rho, numtrials,
+    function run_cluster_experiment(rng, k, noise, scale, n_points,
+                                    kernel, bandwidth, eta, rho, numtrials,
                                     plot_only, destination, readme)
         
         logstr  = "rng       = "*string(rng)*"\n"
         logstr *= "k         = "*string(k)*"\n"
         logstr *= "noise     = "*string(noise)*"\n"
+        logstr *= "scale     = "*string(scale)*"\n"
         logstr *= "n_points  = "*string(n_points)*"\n"
         logstr *= "kernel    = "*kernel*"\n"
         logstr *= "bandwidth = "*string(bandwidth)*"\n"
@@ -100,23 +102,15 @@ if !plot_only
         
         fprintln("generating dataset...")
 
-        data    = zeros(2, N)       # stores data points
-        labels  = zeros(Int64, N)   # stores true labels
+        data    = zeros(k, N)       # stores data points
+        # labels  = zeros(Int64, N)   # stores true labels
         sqnorms = zeros(N)
-        w       = Weights(1:k)
         
-        for i = 1:N
-            l          = rand(rng, 1:k)
-            labels[i]  = l
-
-            t = 2*pi*rand(rng)
-            
-            while rand(rng) > sqrt(.5*(1 + cos(t)^2))
-                t = 2*pi*rand(rng)
-            end
-
-            data[:,i]  = [1.5*pi*l + t, sin(t)] + noise*randn(rng, 2)
-            sqnorms[i] = norm(data[:,i])^2
+        for j = 1:N
+            i          = rand(rng, 1:k)
+            data[i,j]  = scale
+            data[:,j] += noise*randn(rng, k)
+            sqnorms[j] = norm(data[:,j])^2
         end
 
         fprintln("calculating adjacency matrix degrees...")
@@ -142,7 +136,7 @@ if !plot_only
             fprintln("-------------------------------------")
             fprintln("    selecting data points...")
 
-            samp = 1:N #randperm(rng, N)[1:n]
+            samp = randperm(rng, N)[1:n]
 
             fprintln("    approximating normalized adjacency matrix...")
 
@@ -198,59 +192,61 @@ if !plot_only
             s2 = svd(V[p_geqp3, :]')
             Q  = s2.U*s2.Vt
 
-            # classifying the data
+            # classifying the data into mixture components
+
             mul!(A, Q, V')
             broadcast!(abs, A, A)
-            learned_labels_raw = argmax(A, dims = 1)
-            learned_labels     = [learned_labels_raw[i][1] for i = 1:n]
+            labels_raw = argmax(A, dims = 1)
+            labels     = [labels_raw[i][1] for i = 1:n]
+
+            fprintln("    measuring cluster accuracy...")
+
+            means = zeros(k, k)
+            vars  = zeros(k)
+
+            for i = 1:k
+                idx        = (labels .== i)
+                num_idx    = length(idx)
+
+                # finding the mean and variance for the given mixture component
+
+                means[:,i] = mean(data[:,samp[idx]], dims = 2)
+                
+                for j = 1:n
+                    idx[j]  || continue
+                    vars[i] += norm(data[:,samp[j]] - means[:,i])^2/(num_idx - 1)
+                end
+
+                # multivariate Gaussian pdf for this mixture component
+                p(x) = exp(-.5*norm(x - means[:,i])^2/vars[i])/(sqrt(2*pi*vars[i])^k)
+                
+                # E[p(X)] where X ~ Gaussian(means[i], vars[i]*I)
+                E = (.5/sqrt(pi*vars[i]))^k
+
+                for j = 1:n
+                    idx[j]  || continue
+                    cluster_skill[n_idx] += p(data[:,samp[j]])/(n*E)
+                end
+            end
 
             ### BEGIN DEBUGGING BLOCK
                 CairoMakie.activate!(visible = false, type = "pdf")
-                fig = Figure(size = (1200, 600))
+                fig = Figure(size = (700, 700))
                 plt = Axis(fig[1,1])
 
-                for l = 1:k
-                    label_idx = (learned_labels .== l)
-                    scatter!(plt, data[:,label_idx])
+                for i = 1:k
+                    idx = (labels .== i)
+                    scatter!(plt, data[:, samp[idx]])
                 end
 
                 save(destination*"_plot.pdf", fig)
             ### END DEBUGGING BLOCK
 
-            fprintln("    finding learned/true labels correspondence...")
-
-            match_table = zeros(k, k)
-
-            for i = 1:n
-                l = learned_labels[i]
-                match_table[l, labels[samp[i]]] += 1
-            end
-
-            label_perm = zeros(Int64, k)
-
-            for l = 1:k
-                idx                = findmax(match_table)[2]
-                label_perm[idx[1]] = idx[2]
-
-                fill!(view(match_table, idx[1], :), -1.)
-                fill!(view(match_table, :, idx[2]), -1.)
-            end
-
-            fprintln("    measuring cluster accuracy...")
-
-            for i = 1:n
-                l = learned_labels[i]
-                
-                if(label_perm[l] == labels[samp[i]])
-                    cluster_skill[n_idx] += 1/n
-                end
-            end
-
-            @save destination*"_data.jld2" cceqr_cycles cceqr_avgblk cceqr_active cluster_skill data A labels learned_labels label_perm
+            @save destination*"_data.jld2" cceqr_cycles cceqr_avgblk cceqr_active cluster_skill means vars
         end
     end
 
-    run_cluster_experiment(rng, k, noise, n_points, kernel, bandwidth, eta, rho, numtrials,
+    run_cluster_experiment(rng, k, noise, scale, n_points, kernel, bandwidth, eta, rho, numtrials,
                            plot_only, destination, readme)
 end
 
@@ -258,7 +254,7 @@ end
 ######################## PLOTTING ########################################
 ##########################################################################
 
-@load destination*"_data.jld2" cceqr_cycles cceqr_avgblk cceqr_active cluster_skill data A labels learned_labels label_perm
+@load destination*"_data.jld2" cceqr_cycles cceqr_avgblk cceqr_active cluster_skill means vars
 
 println("CLUSTER SKILL:")
 display(cluster_skill)
