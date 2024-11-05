9f70a106874d6facb54f8391dea9beb555830586
Robin Armstrong <rja243@cornell.edu>
2024-11-04 19:34:46 -0500

    Added an experiment timing algorithms on Hadamard matrices.

---
+++ src/experiments/hadamard/run.jl
@@ -0,0 +1,129 @@
+using LinearAlgebra
+using CairoMakie
+using StatsBase
+using Hadamard
+using JLD2
+
+include("../../algorithms/cceqr.jl")
+
+##########################################################################
+######################## SCRIPT PARAMETERS ###############################
+##########################################################################
+
+k         = 32                                       # must be a multiple of 2
+n_range   = round.(Int64, exp2.(range(6, 20, 15)))   # must be multiples of 2
+rho       = 1e-2
+numtrials = 10
+readme    = "Comparing GEQP3 and CCEQR on Gaussian random test matrices of various sizes."
+
+plot_only   = false
+destination = "src/experiments/hadamard/hadamard_test"
+
+##########################################################################
+######################## DATA GENERATION #################################
+##########################################################################
+
+if !plot_only
+    function fprintln(s)
+        println(s)
+        flush(stdout)
+    end
+
+    function run_gaussian_experiment(k, n_range, rho, numtrials, readme, destination)
+        # recording information about this experiment
+
+        logstr  = "k           = "*string(k)*"\n"
+        logstr *= "n_range     = "*string(n_range)*"\n"
+        logstr *= "rho         = "*string(rho)*"\n"
+        logstr *= "numtrials   = "*string(numtrials)*"\n"
+        logstr *= "\n"*readme*"\n"
+
+        logfile = destination*"_log.txt"
+        touch(logfile)
+        io = open(logfile, "w")
+        write(io, logstr)
+        close(io)
+
+        fprintln(logstr)
+
+        cceqr_runtimes = zeros(length(n_range), numtrials)
+        geqp3_runtimes = zeros(length(n_range), numtrials)
+        total_trials   = length(n_range)*numtrials
+        trial_counter  = 0
+        
+        for n_index = 1:length(n_range)
+            n   = n_range[n_index]
+            d   = div(n, k)
+            tmp = zeros(k, n)
+            A   = zeros(k, n)
+            H   = hadamard(k)
+            
+            for i = 1:k
+                idx_A      = ((i-1)*d + 1):(i*d)
+                A[:,idx_A] = H[:, i]*ones(1, d)
+            end
+            
+            D = Diagonal(reverse(1 .+ 1000*eps()*(1:n)))
+            rmul!(A, D)
+
+            fprintln("\nSHAPE "*string(n_index)*" OF "*string(length(n_range)))
+            fprintln("--------------------")
+
+            copy!(tmp, A)
+            p_geqp3 = qr!(tmp, ColumnNorm()).p
+
+            copy!(tmp, A)
+            p_cceqr, _, _, _ = cceqr!(tmp, rho = rho)
+
+            if(p_geqp3[1:k] != p_cceqr)
+                j = 1
+                while(p_geqp3[j] == p_cceqr[j]) j += 1 end
+
+                expected = p_geqp3[j]
+                got      = p_cceqr[j]
+
+                @save destination*"_failure_data.jld2" A rho j expected got
+                throw(error("incorrect permutation from cceqr"))
+            end
+
+            for trial_index = 1:numtrials
+                trial_counter += 1
+                fprintln("    trial "*string(trial_counter)*" of "*string(total_trials)*"...")
+
+                copy!(tmp, A)
+                geqp3_runtimes[n_index, trial_index] = @elapsed qr!(tmp, ColumnNorm())
+
+                copy!(tmp, A)
+                cceqr_runtimes[n_index, trial_index] = @elapsed cceqr!(tmp, rho = rho)
+            end
+
+            @save destination*"_data.jld2" k n_range geqp3_runtimes cceqr_runtimes
+        end
+    end
+
+    A = run_gaussian_experiment(k, n_range, rho, numtrials, readme, destination)
+end
+
+##########################################################################
+######################## PLOTTING ########################################
+##########################################################################
+
+@load destination*"_data.jld2" k n_range geqp3_runtimes cceqr_runtimes
+
+geqp3_mean_times = vec(mean(geqp3_runtimes, dims = 2))
+cceqr_mean_times = vec(mean(cceqr_runtimes, dims = 2))
+
+CairoMakie.activate!(visible = false, type = "pdf")
+fig = Figure(size = (500, 300))
+
+runtimes = Axis(fig[1,1],
+                title  = "Runtimes on Hadamard Adversaries",
+                xlabel = "Number of Columns ("*string(k)*" Rows)",
+                ylabel = "Runtime (s)",
+                xscale = log10,
+                yscale = log10)
+
+lines!(runtimes, n_range, geqp3_mean_times, color = :red, label = "GEQP3")
+lines!(runtimes, n_range, cceqr_mean_times, color = :blue, label = "CCEQR")
+axislegend(runtimes, position = :lt)
+save(destination*"_plot.pdf", fig)
